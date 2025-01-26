ec15a0e3038d6b785bf69b5194647b833268e88d
Angel75Git <aduboncenteno@usf.edu>
2025-01-26 15:00:28 -0500

    Task 3 - Employee Records

--- cc_2.js
+++ cc_2.js
@@ -10,3 +10,15 @@ scores[1] = 85
 console.log(scores)
 let total = scores.reduce((sum, price) => sum + price, 0);
 console.log("Average:", total / scores.length)
+
+let employee = {
+    name: "Greg",
+    age: 23,
+    department: "Marketing",
+    isActive: true
+}
+employee.department = "HR"
+
+employee.position = "Senior Manager"
+
+console.log(employee)
