a7ea8b34fe7bd1d0c1c307b5af34bdb4a93bc90e
Angel75Git <aduboncenteno@usf.edu>
2025-01-26 11:04:47 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -4,3 +4,9 @@ console.log(products)
 
 products.pop();
 console.log(products)
+
+let scores = [84,82,95,90,79]
+scores[1] = 85
+console.log(scores)
+let total = scores.reduce((sum, price) => sum + price, 0);
+console.log("Average:", total / scores.length)
